4687ad95df3efe423c9955ff66ff81df3f6d6f48
fmheu <fhchl@users.noreply.github.com>
2018-04-04 14:30:48 +0200

    Preprocess pipeline in import and calc of TFs.

--- measuretf.py
+++ measuretf.py
@@ -1,9 +1,10 @@
 """Measurement signals."""
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 from pathlib import Path
-from scipy.signal import hann, butter, lfilter
+from scipy.signal import hann, butter, lfilter, get_window, convolve
 from scipy.io import loadmat
 
 from tqdm import tqdm
@@ -35,11 +36,11 @@ def exponential_sweep(T, fs, tfade=0.05, f_start=None, f_end=None):
        Paris, France, 2000, pp. 1–15.
 
     """
-    ntaps = int(np.round(T * fs))
+    n_tap = int(np.round(T * fs))
 
     # start and stop frequencies
     if f_start is None:
-        f_start = fs / ntaps
+        f_start = fs / n_tap
     if f_end is None:
         f_end = fs / 2
 
@@ -47,7 +48,7 @@ def exponential_sweep(T, fs, tfade=0.05, f_start=None, f_end=None):
     w_end = 2 * np.pi * f_end
 
     # constuct sweep
-    t = np.linspace(0, T, ntaps)
+    t = np.linspace(0, T, n_tap)
     sweep = np.sin(w_start * T / np.log(w_end / w_start) *
                    (np.exp(t / T * np.log(w_end / w_start)) - 1))
     sweep = sweep * 0.95  # some buffer in amplitude
@@ -139,7 +140,7 @@ def multichannel_serial_sound(sound, n_ch, reference=False):
     return msound
 
 
-def transfer_function(ref, meas, axis=-1):
+def transfer_function(ref, meas, ret_time=True, axis=-1):
     """Compute transfer-function between time domain signals.
 
     Parameters
@@ -160,11 +161,14 @@ def transfer_function(ref, meas, axis=-1):
     R = np.fft.rfft(ref, axis=axis)  # no need for normalization because
     Y = np.fft.rfft(meas, axis=axis)  # of division
     H = Y / (R + np.spacing(1))  # avoid devided-by-zero errors
-    h = np.fft.irfft(H, axis=axis, n=n)
-    return h
+    if ret_time:
+        h = np.fft.irfft(H, axis=axis, n=n)
+        return h
+    else:
+        return H
 
 
-def multi_transfer_function(recs, ref_ch=0):
+def multi_transfer_function(recs, ref_ch=0, ret_time=True):
     """Transfer-function between multichannel recording and reference channels.
 
     Parameters
@@ -181,12 +185,16 @@ def multi_transfer_function(recs, ref_ch=0):
         domain.
     """
     n_ch, n_ls, n_avg, n_tap = recs.shape
-    tfs = np.zeros((n_ch, n_ls, n_tap))
+    if ret_time:
+        tfs = np.zeros((n_ch, n_ls, n_tap))
+    else:
+        tfs = np.zeros((n_ch, n_ls, n_tap // 2 + 1))
     for avg in range(n_avg):
         for ch in range(n_ch):
             for ls in range(n_ls):
                 tfs[ch, ls] += transfer_function(recs[ref_ch, ls, avg],
-                                                 recs[ch, ls, avg]) / n_avg
+                                                 recs[ch, ls, avg],
+                                                 ret_time=ret_time) / n_avg
     return tfs
 
 
@@ -217,7 +225,7 @@ def header_info(fname):
     return fs, n_tap, n_ch
 
 
-def load_recordings(fname, n_ls=1, n_avg=1):
+def load_mat_recording(fname, n_ls=1, n_avg=1, fullout=False):
     """Load multichannel Time Data Recording into ndarray.
 
     Parameters
@@ -226,7 +234,7 @@ def load_recordings(fname, n_ls=1, n_avg=1):
         Name of the mat file. Can also pass open file-like object. Reference
         channel is assumed to be recorded in first channel.
     n_ls : int, optional
-        Number of simultaneously recorded output channels.
+        Number of simultaneously, in series recorded output channels.
     n_avg : int, optional
         Number of recorded averages.
 
@@ -249,30 +257,141 @@ def load_recordings(fname, n_ls=1, n_avg=1):
         temp = np.array(np.split(data['Channel_{}_Data'.format(i + 1)], n_avg))
         recs[i] = np.array(np.split(temp, n_ls, axis=1))  # (n_ls, n_avg, N)
 
+    if fullout:
+        return recs, fs, n_ch, n_tap
+
     return recs, fs
 
 
-def transfer_functions_from_recordings(fp,
-                                       n_och,
-                                       n_meas,
-                                       H_comp=None,
-                                       fformat='Recording-{}.mat',
-                                       n_avg=1,
-                                       ref_ch=0,
-                                       lowpass_lim=None,
-                                       take_T=None):
+def load_npz_recording(fname, n_ls=1, n_avg=1, fullout=False):
+    """Load multichannel Time Data Recording into ndarray.
+
+    Parameters
+    ----------
+    fname : str
+        Name of the mat file. Can also pass open file-like object. Reference
+        channel is assumed to be recorded in first channel.
+    n_ls : int, optional
+        Number of simultaneously, in series recorded output channels.
+    n_avg : int, optional
+        Number of recorded averages.
+
+    Returns
+    -------
+    recs : ndarray, shape (n_ch, n_ls, n_avg, n_tap)
+        Recordings, sliced into averages and output channels.
+    fs : int
+        Sampling frequency.
+    """
+
+    with np.load(fname) as data:
+        fs = data['fs']
+        n_otap = data['n_tap']
+        n_ch = data['n_ch']
+        orecs = data['recs']
+
+    n_tap = n_otap / n_ls / n_avg
+    if n_tap.is_integer():
+        n_tap = int(n_tap)
+    else:
+        raise ValueError()
+
+    recs = np.zeros((n_ch, n_ls, n_avg, n_tap))
+    for i in range(n_ch):
+        # shape (N*n_avg*n_ls, ) -> (n_avg, N*n_ls)
+        temp = np.array(np.split(orecs[i], n_avg))
+        recs[i] = np.array(np.split(temp, n_ls, axis=1))  # (n_ls, n_avg, N)
+
+    if fullout:
+        return recs, fs, n_ch, n_tap
+
+    return recs, fs
+
+
+def convert_TDRmat_recording_to_npz(fname, output_folder=None):
+    """Convert .mat recording from Time Data recorder into npz format.
+
+    Parameters
+    ----------
+    fname : str or Path
+        File path
+    n_ls : int, optional
+        Number of simultaneously, in series recorded output channels.
+    n_avg : int, optional
+        Number of recorded averages.
+    folder : None or Path, optional
+        Save in this folder, instead of same folder as fname (default)
+    """
+    data = loadmat(fname, struct_as_record=False, squeeze_me=True)
+    fh = data['File_Header']
+    n_ch = int(fh.NumberOfChannels)
+    fs = int(float(fh.SampleFrequency))  # only int() doesn't work ...
+    n_tap = int(int(fh.NumberOfSamplesPerChannel))
+
+    recs = np.zeros((n_ch, n_tap))
+
+    for i in range(n_ch):
+        recs[i] = np.array(data['Channel_{}_Data'.format(i + 1)])
+
+    # remove .mat suffix
+    path = Path(fname)
+    parent = Path(output_folder) if output_folder is not None else path.parent
+    newpath = parent / path.stem
+
+    np.savez(newpath, recs=recs, fs=fs, n_ch=n_ch, n_tap=n_tap)
+    #import ipdb; ipdb.set_trace()
+
+
+def folder_convert_TDRmat_recording_to_npz(
+        path,
+        output_folder=None,
+):
+    """Convert all .mat recording in folder into npz format.
+
+    Parameters
+    ----------
+    path : str or Path
+        Folder path
+    n_ls : int, optional
+        Number of simultaneously, in series recorded output channels.
+    n_avg : int, optional
+        Number of recorded averages.
+    folder : None or Path, optional
+        Save in this folder, instead of same folder as fname (default)
+    """
+    files = Path(path).glob('*.mat')
+    for f in tqdm(list(files)):
+        convert_TDRmat_recording_to_npz(f, output_folder=output_folder)
+
+
+def transfer_functions_from_recordings(
+        fp,
+        n_ls,
+        n_meas,
+        fformat='Recording-{}.mat',
+        n_avg=1,
+        ref_ch=0,
+        lowpass_lim=None,
+        lowpass_butt=None,
+        twindow=None,
+        take_T=None,
+        H_comp=None,
+        T_comp=None,
+):
     """Calculate transfer-functions from a set of recordings inside a folder.
 
     Parameters
     ----------
     fp : str or Path
         Path to folder.
-    n_och : int
+    n_ls : int
         Number of serially measured channels.
     n_meas : int
         Total number of recordings
-    H_comp : None or ndarray, shape (n_ich - 1, nf), optional
+    H_comp : None or ndarray, shape (n_ch - 1, nf), optional
         If present, apply a compensation filter to each single recording.
+    T_comp : None or float
+        Length of H_comp in time domain in seconds.
     fformat : str, optional
         Recording file naming. Must include one '{}' to enumerate.
     n_avg : int, optional
@@ -281,51 +400,113 @@ def transfer_functions_from_recordings(fp,
         Index of reference channel
     lowpass_lim : None or Tuple, optional
         Tuple (fl, fu) that defines transition range of lowpass filter.
+    twindow : None or tuple
+        Specify time domain window.
     take_T : float or None, optional
         If float, only take the take_T first seconds of the impulse responses.
+        if twindiow==None: Hann window in last 5%.
 
     Returns
     -------
-    ndarray, shape (n_meas, n_ich - 1, n_och, ntaps // 2 + 1)
+    ndarray, shape (n_meas, n_ch - 1, n_ls, n_tap // 2 + 1)
         Transfer-functions. Possibly lowpass filtered
     """
     fpath = Path(fp)
 
     # read meta data from first recording
-    fs, ntaps, n_ich = header_info(fpath / fformat.format(1))
+    # TODO: use load_recording funcs here
+    fname = fpath / fformat.format(1)
+    if fname.suffix == '.mat':
+        fs, n_tap, n_ch = header_info(fname)
+    elif fname.suffix == '.npz':
+        with np.load(fname) as data:
+            fs = data['fs']
+            n_tap = data['n_tap']
+            n_ch = data['n_ch']
+    else:
+        raise ValueError('Neither mat nor npz file.')
 
     if take_T is not None:
         # only take first take_T seconds of impulse responses
-        ntaps = take_T * fs
+        n_tap = int(np.ceil(take_T * fs))
     else:
-        ntaps = int(ntaps / n_och / n_avg)
+        n_tap = int(n_tap / n_ls / n_avg)
 
-    H = np.zeros((n_meas, n_ich - 1, n_och, ntaps // 2 + 1), dtype=complex)
+    H = np.zeros((n_meas, n_ch - 1, n_ls, n_tap // 2 + 1), dtype=complex)
 
     for i in tqdm(np.arange(n_meas)):
-        fname = fpath / 'Recording-{}.mat'.format(i + 1)
+        fname = fpath / fformat.format(i + 1)
 
-        temp, fs = load_recordings(fname, n_och, n_avg=n_avg)
-        temp = multi_transfer_function(temp, ref_ch=ref_ch)
+        # load time domain recordings
+        if Path(fname).suffix == '.mat':
+            temp, fs = load_mat_recording(fname, n_ls=n_ls, n_avg=n_avg)
+        elif Path(fname).suffix == '.npz':
+            temp, fs = load_npz_recording(fname, n_ls=n_ls, n_avg=n_avg)
+        else:
+            raise ValueError()
+
+        # compute transfer-function in time domain
+        temp = multi_transfer_function(temp, ref_ch=ref_ch, ret_time=True)
 
         # exclude reference channel
         temp = temp[1:]
 
+        if H_comp is not None:
+            # time crop to same length as compensation filter
+            n_comp = int(T_comp * fs)
+            temp = temp[:, :, :n_comp]
+            Temp = np.fft.rfft(temp)
+            # apply compensation filter
+            Temp *= H_comp[:, None, :]
+            temp = np.fft.irfft(Temp, n=n_comp)
+
         if take_T is not None:
             # only take first take_T seconds
-            temp = temp[:, :, :ntaps]
+            temp = temp[:, :, :n_tap]
+            if twindow is None:
+                # time window the tail
+                nwin = int(round(take_T * 0.05 * fs))
+                w = hann(2 * nwin)[nwin:]
+                temp[:, :, -nwin:] *= w
 
         if lowpass_lim is not None:
-            # filter out HF noise
+            # filter out HF noise with zero phase frequency domain window
             temp = lowpass_by_frequency_domain_window(fs, temp, *lowpass_lim)
+
+        if lowpass_butt is not None:
+            # filter HF noise with butterworth
+            order, cutoff = lowpass_butt
+            b, a = butter(order, cutoff / (fs / 2), 'low')
+            temp = lfilter(b, a, temp, axis=-1)
+
+        if twindow is not None:
+            T = time_window(fs, n_tap, *twindow)
+            temp *= T
 
         H[i] = np.fft.rfft(temp)
 
-        if H_comp is not None:
-            # apply compensation filter
-            H[i] = H[i] * H_comp[:, None, :]
+    return H, fs, n_tap
 
-    return H, fs
+
+def cut_recording(fname, cuts, names=None, remove_orig=False, outfolder=None):
+    """Cut recordings at samples.
+    """
+    data = np.load(fname)
+    recs = np.split(data['recs'], cuts, axis=-1)
+    path = Path(fname)
+
+    for i, r in enumerate(recs):
+        add = '-{}'.format(names[i] if names is not None else i)
+        parent = Path(outfolder) if outfolder is not None else path.parent
+        newpath = parent / (path.stem + add)
+        np.savez(
+            newpath, recs=r, fs=data['fs'], n_ch=r.shape[0], n_tap=r.shape[1])
+
+    data.close()
+
+    if remove_orig:
+        # remove original files
+        path.unlink()
 
 
 """NON_LINEAR"""
@@ -355,10 +536,10 @@ def exponential_sweep_harmonic_delay(T, fs, N, f_start=None, f_end=None):
        with a swept-sine techniqueMinnaar, Pauli,” in Proc. AES 108th conv,
        Paris, France, 2000, pp. 1–15.
     """
-    ntaps = int(np.round(T * fs))
+    n_tap = int(np.round(T * fs))
 
     if f_start is None:
-        f_start = fs / ntaps
+        f_start = fs / n_tap
     if f_end is None:
         f_end = fs / 2
 
@@ -386,7 +567,7 @@ def harmonic_spectrum(r, fs, order=10):
         The energy of the nth order harmonics in the impules response.
     """
     n = r.size
-    orders = np.arange(1, order+2)
+    orders = np.arange(1, order + 2)
     T = n / fs
 
     # find max and circshift it to tap 0
@@ -407,8 +588,8 @@ def harmonic_spectrum(r, fs, order=10):
 
     # higher order
     for i in orders[:-2]:
-        n_start = int(round((dns[i+1] + dns[i]) / 2))
-        n_end = int(round((dns[i] + dns[i-1]) / 2))
+        n_start = int(round((dns[i + 1] + dns[i]) / 2))
+        n_end = int(round((dns[i] + dns[i - 1]) / 2))
         e[i] = np.sum(np.abs(r[n_start:n_end])**2)
 
     return e
@@ -490,7 +671,62 @@ def lowpass_by_frequency_domain_window(fs, x, fstart, fstop, axis=-1):
     X_windowed[stop:] = 0
     X_windowed = np.moveaxis(X_windowed, 0, axis)
 
-    return np.fft.irfft(X_windowed)
+    return np.fft.irfft(X_windowed, n=n)
+
+
+def time_window(fs, n, startwindow, stopwindow, window='hann'):
+    """Create a time window.
+    """
+
+    times = time_vector(n, fs)
+    twindow = np.ones(n)
+
+    if startwindow is not None:
+        # start window
+        samples = [find_nearest(times, t)[1] for t in startwindow]
+        length = samples[1] - samples[0]
+        w = get_window(window, 2 * length, fftbins=False)[:length]
+        twindow[:samples[0]] = 0
+        twindow[samples[0]:samples[1]] = w
+
+    if stopwindow is not None:
+        # stop window
+        samples = [find_nearest(times, t)[1] for t in stopwindow]
+        length = samples[1] - samples[0]
+        w = get_window(window, 2 * length, fftbins=False)[length:]
+        twindow[samples[0] + 1:samples[1] + 1] = w
+        twindow[samples[1] + 1:] = 0
+
+    return twindow
+
+
+def mutliconvolve(sound, h, plot=False):
+    """Convolve signel channel sound with multichannel impulse response.
+    """
+    nch = h.shape[-1]
+    nfilt = h.shape[0]
+    nsound = sound.shape[0]
+
+    # convolve sounds
+    convsound = np.zeros((nfilt + nsound - 1, nch + 1))
+    for i in range(nch):
+        convsound[:, i] = convolve(sound, h[:, i])
+
+    # convolve reference with unit filter
+    h_unit = np.zeros(h.shape[0])
+    h_unit[0] = 1
+    convsound[:, -1] = convolve(sound, h_unit)
+
+    # normalize
+    convsound *= 1 / np.max(np.abs(convsound)) * 0.95
+
+    if plot:
+        fig, axes = plt.subplots(nrows=nch + 1, figsize=(20, 30))
+        for i, ax in enumerate(axes):
+            ax.plot(convsound[:, i])
+            ax.set_ylim((-1, 1))
+
+    return convsound
 
 
 """FFT FUNCS"""
@@ -521,6 +757,26 @@ def frequency_vector(n, fs, sided='single'):
     else:
         raise ValueError('Invalid value for sided.')
     return f
+
+
+def time_vector(n, fs):
+    """Time values of filter with n taps sampled at fs.
+
+    Parameters
+    ----------
+    n : int
+        number of taps in FIR filter
+    fs : int
+        sampling frequency in Hertz
+
+    Returns
+    -------
+    (n) ndarray
+        times in seconds
+
+    """
+    T = 1 / fs
+    return np.arange(n) * T
 
 
 def amplitude_spectrum(x, axis=-1, norm=True):
