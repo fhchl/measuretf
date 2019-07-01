5d35b5968be79f31ddf8bf8e3fd082b03f165e63
fmheu <fhchl@users.noreply.github.com>
2019-07-01 19:55:35 +0200

    fix: faster, memory efficient time code

--- measuretf/timecode.py
+++ measuretf/timecode.py
@@ -5,175 +5,49 @@ Created on Thu Jun 20 23:18:41 2019
 @author: minson
 """
 
-import sys
 import numpy as np
-import scipy.io.wavfile as wavfile
+import soundfile as sf
 from pathlib import Path
-from datetime import datetime
+from datetime import datetime, timezone
 from scipy import signal
-import os
-import fnmatch
+
+import matplotlib.pyplot as plt
+import warnings
+from joblib import Parallel, delayed
+
+from tqdm import tqdm
 
 
-def resync(folder_name, start_time, end_time):
-    """Read multiple wav recordings *rc.wav from the given folder,
-    Slice files for given interval (in time).
-    Save as .npz
-
-    Parameters
-    ----------
-    folder_name : folder with fullpath, ex) 'D:/ASFC/Roskilde/darkzone'
-
-    save
-    -------
-    saves namelist_rc, namelist_tc, audio chunks matrix as .npz file
-    filename: startT_endT_aligned.npz
-
-    """
-
-    # Check whether the folder actually exists. Warn User if not
-    if os.path.isdir(folder_name) is False:
-        errMsg = "Error: The following folder does not exist:{}".format(folder_name)
-        sys.exit(errMsg)
-
-    # Get Recordings from the folder
-    listOfFiles = os.listdir(folder_name)
-
-    pattern_rc = "*rc.wav"  # find file with *tc.wav
-    namelist_rc = []
-    for entry in listOfFiles:
-        if fnmatch.fnmatch(entry, pattern_rc):
-            namelist_rc.append(entry)
-
-    # Q. what if multiple tc inside a single folder?
-    pattern_tc = "*tc.npy"  # find TC file with *tc.npy
-    namelist_tc = []
-    for entry in listOfFiles:
-        if fnmatch.fnmatch(entry, pattern_tc):
-            namelist_tc.append(entry)
-            fname_tc = Path(folder_name, entry)
-            TC_list = np.load(fname_tc)
-            print("Time-stamp file {} loaded.".format(entry))
-
-    minT = TC_list[0][0]
-    maxT = TC_list[-1][0]
-    minT_Tobj = datetime.strptime(minT, "%Y-%m-%dT%H:%M:%SZ")
-    maxT_Tobj = datetime.strptime(maxT, "%Y-%m-%dT%H:%M:%SZ")
-    ST_Tobj = datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%SZ")
-    ET_Tobj = datetime.strptime(end_time, "%Y-%m-%dT%H:%M:%SZ")
-    num_minT = int(datetime.timestamp(minT_Tobj))
-    num_maxT = int(datetime.timestamp(maxT_Tobj))
-    num_ST = int(datetime.timestamp(ST_Tobj))
-    num_ET = int(datetime.timestamp(ET_Tobj))
-
-    if (
-        (num_ST > num_ET)
-        or (num_minT > num_ST)
-        or (num_ST >= num_maxT)
-        or (num_minT >= num_ET)
-        or (num_ET >= num_maxT)
-    ):
-        errMsg2 = "Invalid start_time or end_time"
-        sys.exit(errMsg2)
-
-    # Find matching sample indices
-    nth_arr_ST = np.where(TC_list == start_time)[0][0]  # should be in np array
-    nth_arr_ET = np.where(TC_list == end_time)[0][0]
-
-    #    idx_ST = int(TC_list[nth_arr_ST][1])
-    #    idx_ET = int(TC_list[nth_arr_ET][1])
-
-    recs_resampled = np.zeros((48000 * (nth_arr_ET - nth_arr_ST), len(namelist_rc)))
-    for k in range(len(namelist_rc)):
-        temp_name = namelist_rc[k]
-        temp_fname = Path(folder_name, temp_name)
-        print("Now reading audio file {}\n.".format(temp_name))
-
-        # Read Audio
-        fs, yy = wavfile.read(temp_fname)
-        yy_resampled = np.zeros((0, 1))
-
-        for j in range(nth_arr_ST, nth_arr_ET):
-            temp_idx = int(TC_list[j][1])
-            temp_idx_last = int(TC_list[j + 1][1])
-            temp_crop = yy[temp_idx:temp_idx_last]
-            yy_crop_dn = signal.resample(temp_crop, 8000)
-            yy_crop_up = signal.resample(yy_crop_dn, 48000)
-            yy_resampled = np.append(yy_resampled, yy_crop_up)
-
-        recs_resampled[:, k] = yy_resampled
-
-    # Save audio chunks for given start_time, end_time
-    #    ST_HHMMSSTddmmyyyy = ST_Tobj.strftime("%H%M%ST%d%m%Y")  # ST
-    #    ET_HHMMSSTddmmyyyy = ET_Tobj.strftime("%H%M%ST%d%m%Y")  # ET
-    #    dirname = folder_name
-    #    RC_filename = ST_HHMMSSTddmmyyyy + "_" + ET_HHMMSSTddmmyyyy + "_aligned"
-    #    suffix = ".npz"
-    #    RC_fullname = Path(dirname, RC_filename).with_suffix(suffix)
-    #    np.savez(RC_fullname, namelist_rc, namelist_tc, recs_resampled)
-    #    print("Recordings aligned, {} saved.\n".format(RC_fullname))
-
-    return namelist_rc, namelist_tc, recs_resampled
-
-
-# Execution Code
-# folder_name = "D:/ASFC/Roskilde/darkzone/trash/test"
-# out1 = resync(folder_name, "2019-06-13T12:44:40Z", "2019-06-13T12:44:42Z")
-
-
-# Decode IRIG-B signal
-# Minho Song, Technical University of Denmark, DK
-
-
-
-
-def tc2tstamp(folder_name):
-    """Read TimeCode recording *tc.wav from the given folder,
-    Extracts time code in YYYY-MM-DDThh:mm:ssZ format.
-    Save as .npy: [(1st sec, sample #), (2nd sec, sample #), ...]
+def timestamps_from_irigb(irigb, fs):
+    """Find time stamps in irig-B signal.
 
     Parameters
     ----------
-    folder_name : folder with fullpath, ex) 'D:/ASFC/Roskilde/darkzone'
+    irigb : ndarray
+        IRIG-B signal.
+    fs : int
+        Sample rate if IRIG-B signal.
 
-    save
+    Returns
     -------
-    saves .npy file
-    filename: startT_endT_tc.npy
+    list
+        List of (timestamp, index) tuples.
+
     """
+    # preprocess: turn into binary signal
+    # TODO: remove low frequency components?
+    positive = irigb >= 0
+    irigb[positive] = 1
+    irigb[np.logical_not(positive)] = 0
 
-    # Check whether the folder actually exists. Warn User if not
-    if os.path.isdir(folder_name) is False:
-        errMsg = "Error: The following folder does not exist:{}".format(folder_name)
-        sys.exit(errMsg)
+    # Compute pulse widths:
+    x_edge = np.diff(irigb)
 
-    # Get Time Code recording from the folder
-    listOfFiles = os.listdir(folder_name)
+    # Indices of 1-seg start
+    edge_up = np.where(x_edge == 1)[0] + 1  # 1 less than the true value
 
-    # Q. what if multiple tc inside a single folder?
-    pattern = "*tc.wav"  # find file with *tc.wav
-    namelist = []
-    for entry in listOfFiles:
-        if fnmatch.fnmatch(entry, pattern):
-            namelist.append(entry)
-            print("Now reading {}\n".format(entry))
-
-    # Read TC in wav file
-    data_folder = Path(folder_name)
-    file_FullName = data_folder / namelist[0]
-
-    fs, irigB = wavfile.read(file_FullName)
-    irigB_Z = irigB / 32768  # Raw TC
-    irigB_Z_clean = np.ceil(irigB_Z)  # Raw TC to clean Pulse
-    x_edge = np.diff(irigB_Z_clean)
-
-    # Indices of 1-seg start from irigB_Z_clean
-    edge_up = np.where(x_edge == 1)[0]  # 1 less than the true value
-    edge_up = [x + 1 for x in edge_up]
-
-    # Indices of 0-seg start from irigB_Z_clean
-    edge_dn = np.where(x_edge == -1)[0]  # 1 less than the true value
-    edge_dn = [x + 1 for x in edge_dn]
+    # Indices of 0-seg start
+    edge_dn = np.where(x_edge == -1)[0] + 1  # 1 less than the true value
 
     if edge_dn[0] < edge_up[0]:
         edge_dn = edge_dn[1:]
@@ -183,65 +57,86 @@ def tc2tstamp(folder_name):
         if len(edge_dn) != len(edge_up):
             edge_up = edge_up[:-1]
 
-    pls_wdt = np.subtract(edge_dn, edge_up) / fs  # Pulse width in s
-    pls_wdt_sft = [y - 0.003 for y in pls_wdt]
+    pls_wdt = np.round((edge_dn - edge_up) / fs * 1000).astype(int)  # Pulse width in ms
+
+    allowed_pulse_widths = reference_width, ones_width, zeros_width = (8, 5, 2)
+
+    pls_wdt_binary = pls_wdt.copy()
+    pls_wdt_binary[pls_wdt_binary == ones_width] = 1
+    pls_wdt_binary[pls_wdt_binary == zeros_width] = 0
 
     # Decoding the Time Code (TC)
-    TC_list = []  # list(array1, array2)...
+    timestamps = []  # list(array1, array2)...
 
     for ii in range(0, len(pls_wdt) - 58):  # Consider "Incomplete last second"
-        if pls_wdt[ii] > 0.0077 and pls_wdt[ii + 1] > 0.0077:
+        if pls_wdt[ii] == reference_width and pls_wdt[ii + 1] == reference_width:
+            # start sequence detected
 
-            Second = int(
-                1 * np.ceil(pls_wdt_sft[ii + 2])
-                + 2 * np.ceil(pls_wdt_sft[ii + 3])
-                + 4 * np.ceil(pls_wdt_sft[ii + 4])
-                + 8 * np.ceil(pls_wdt_sft[ii + 5])
-                + 10 * np.ceil(pls_wdt_sft[ii + 7])
-                + 20 * np.ceil(pls_wdt_sft[ii + 8])
-                + 40 * np.ceil(pls_wdt_sft[ii + 9])
+            if not np.all(np.isin(pls_wdt[ii : ii + 60], allowed_pulse_widths)):
+                warnings.warn(
+                    f"Could not decode timestamp at {ii / 100}s: invalid pulse widths"
+                )
+                # timestamps.append((None, edge_up[ii + 1]))
+                continue
+            elif not np.all(
+                [pls_wdt[ii + i] == reference_width for i in [10, 20, 30, 40, 50, 60]]
+            ):
+                warnings.warn(
+                    f"Could not decode timestamp at {ii / 100}s: marker bits not set correctly"
+                )
+                # timestamps.append((None, edge_up[ii + 1]))
+                continue
+
+            Second = (
+                1 * pls_wdt_binary[ii + 2]
+                + 2 * pls_wdt_binary[ii + 3]
+                + 4 * pls_wdt_binary[ii + 4]
+                + 8 * pls_wdt_binary[ii + 5]
+                + 10 * pls_wdt_binary[ii + 7]
+                + 20 * pls_wdt_binary[ii + 8]
+                + 40 * pls_wdt_binary[ii + 9]
             )
-            Minute = int(
-                1 * np.ceil(pls_wdt_sft[ii + 11])
-                + 2 * np.ceil(pls_wdt_sft[ii + 12])
-                + 4 * np.ceil(pls_wdt_sft[ii + 13])
-                + 8 * np.ceil(pls_wdt_sft[ii + 14])
-                + 10 * np.ceil(pls_wdt_sft[ii + 16])
-                + 20 * np.ceil(pls_wdt_sft[ii + 17])
-                + 40 * np.ceil(pls_wdt_sft[ii + 18])
+            Minute = (
+                1 * pls_wdt_binary[ii + 11]
+                + 2 * pls_wdt_binary[ii + 12]
+                + 4 * pls_wdt_binary[ii + 13]
+                + 8 * pls_wdt_binary[ii + 14]
+                + 10 * pls_wdt_binary[ii + 16]
+                + 20 * pls_wdt_binary[ii + 17]
+                + 40 * pls_wdt_binary[ii + 18]
             )
             # Hours can differ by device setting. UTC is default
-            Hour = int(
-                1 * np.ceil(pls_wdt_sft[ii + 21])
-                + 2 * np.ceil(pls_wdt_sft[ii + 22])
-                + 4 * np.ceil(pls_wdt_sft[ii + 23])
-                + 8 * np.ceil(pls_wdt_sft[ii + 24])
-                + 10 * np.ceil(pls_wdt_sft[ii + 26])
-                + 20 * np.ceil(pls_wdt_sft[ii + 27])
+            Hour = (
+                1 * pls_wdt_binary[ii + 21]
+                + 2 * pls_wdt_binary[ii + 22]
+                + 4 * pls_wdt_binary[ii + 23]
+                + 8 * pls_wdt_binary[ii + 24]
+                + 10 * pls_wdt_binary[ii + 26]
+                + 20 * pls_wdt_binary[ii + 27]
             )
             # Day: nth day in this year
-            Day = int(
-                1 * np.ceil(pls_wdt_sft[ii + 31])
-                + 2 * np.ceil(pls_wdt_sft[ii + 32])
-                + 4 * np.ceil(pls_wdt_sft[ii + 33])
-                + 8 * np.ceil(pls_wdt_sft[ii + 34])
-                + 10 * np.ceil(pls_wdt_sft[ii + 36])
-                + 20 * np.ceil(pls_wdt_sft[ii + 37])
-                + 40 * np.ceil(pls_wdt_sft[ii + 38])
-                + 80 * np.ceil(pls_wdt_sft[ii + 39])
-                + 100 * np.ceil(pls_wdt_sft[ii + 41])
-                + 200 * np.ceil(pls_wdt_sft[ii + 42])
+            Day = (
+                1 * pls_wdt_binary[ii + 31]
+                + 2 * pls_wdt_binary[ii + 32]
+                + 4 * pls_wdt_binary[ii + 33]
+                + 8 * pls_wdt_binary[ii + 34]
+                + 10 * pls_wdt_binary[ii + 36]
+                + 20 * pls_wdt_binary[ii + 37]
+                + 40 * pls_wdt_binary[ii + 38]
+                + 80 * pls_wdt_binary[ii + 39]
+                + 100 * pls_wdt_binary[ii + 41]
+                + 200 * pls_wdt_binary[ii + 42]
             )
             # Year: nth year from 2000
-            Year = int(
-                1 * np.ceil(pls_wdt_sft[ii + 51])
-                + 2 * np.ceil(pls_wdt_sft[ii + 52])
-                + 4 * np.ceil(pls_wdt_sft[ii + 53])
-                + 8 * np.ceil(pls_wdt_sft[ii + 54])
-                + 10 * np.ceil(pls_wdt_sft[ii + 56])
-                + 20 * np.ceil(pls_wdt_sft[ii + 57])
-                + 40 * np.ceil(pls_wdt_sft[ii + 58])
-                + 80 * np.ceil(pls_wdt_sft[ii + 59])
+            Year = 2000 + (
+                1 * pls_wdt_binary[ii + 51]
+                + 2 * pls_wdt_binary[ii + 52]
+                + 4 * pls_wdt_binary[ii + 53]
+                + 8 * pls_wdt_binary[ii + 54]
+                + 10 * pls_wdt_binary[ii + 56]
+                + 20 * pls_wdt_binary[ii + 57]
+                + 40 * pls_wdt_binary[ii + 58]
+                + 80 * pls_wdt_binary[ii + 59]
             )
 
             # LeapYear?
@@ -268,30 +163,175 @@ def tc2tstamp(folder_name):
                     Date = rdays + DaysInMonth[Month - 1]
                     break
 
-            # Our time code is as YYYY-MM-DDTHH:MM:SSZ
-            time = "{:d}-{:02d}-{:02d}T{:02d}:{:02d}:{:02d}Z".format(
-                2000 + Year, Month, Date, Hour, Minute, Second
-            )
+            dt = datetime(Year, Month, Date, Hour, Minute, Second, tzinfo=timezone.utc)
+            timestamps.append((dt, edge_up[ii + 1]))
 
-            TC_list.append((time, edge_up[ii + 1]))
+    return timestamps
+
+
+def tc2tstamp(folder_name):
+    """Read TimeCode recording *tc.wav from the given folder,
+    Extracts time code in YYYY-MM-DDThh:mm:ssZ format.
+    Save as .npy: [(1st sec, sample #), (2nd sec, sample #), ...]
+
+    Parameters
+    ----------
+    folder_name : folder with fullpath, ex) 'D:/ASFC/Roskilde/darkzone'
+
+    save
+    -------
+    saves .npy file
+    filename: startT_endT_tc.npy
+    """
+    folder = Path(folder_name)
+    # Check whether the folder actually exists. Warn User if not
+    if not folder.is_dir():
+        raise FileNotFoundError(folder_name)
+
+    tc_file = next(folder.glob("*tc.wav"))
+    print(f"Reading {tc_file}")
+
+    irigb, fs = sf.read(str(tc_file))
+
+    timestamps = timestamps_from_irigb(irigb, fs)
 
     # Save TC
-    minT = TC_list[0][0]  # Start time of the files
-    min_yyyymmddTHHMMSS = datetime.strptime(minT, "%Y-%m-%dT%H:%M:%SZ")
-    min_HHMMSSTddmmyyyy = min_yyyymmddTHHMMSS.strftime("%H%M%ST%d%m%Y")
-    maxT = TC_list[-1][0]  # End time of the files
-    max_yyyymmddTHHMMSS = datetime.strptime(maxT, "%Y-%m-%dT%H:%M:%SZ")
-    max_HHMMSSTddmmyyyy = max_yyyymmddTHHMMSS.strftime("%H%M%ST%d%m%Y")
-    dirname = folder_name
-    TC_filename = min_HHMMSSTddmmyyyy + "_" + max_HHMMSSTddmmyyyy + "_tc"
-    suffix = ".npy"
-    TC_fn = Path(dirname, TC_filename).with_suffix(suffix)
-    # TC_fn_nPath = TC_fn.replace(os.sep, '/')
-    np.save(TC_fn, TC_list)
-    print("TimeCode {} saved.\n".format(TC_fn))
-    return namelist, TC_fn, TC_list
+    min_time = timestamps[0][0]  # Start time of the files
+    max_time = timestamps[-1][0]  # End time of the files
+    timestamps_filename = (
+        min_time.isoformat() + "_" + max_time.isoformat() + "_timestamps"
+    )
+    timestamps_path = Path(folder_name, timestamps_filename)
+    np.savez(timestamps_path, timestamps=timestamps, samplerate=fs)
+    print(f"Time code saved at {timestamps_path}")
+
+    return timestamps
 
 
-# Test Code
-# folder_name = "D:/ASFC/Roskilde/darkzone/simple_validation/out_sync/dark"
-# out = tc2time(folder_name)  # array
+def sync_to_timestamps(
+    x, timestamps, samplerate, start_time=None, end_time=None, out=None
+):
+    """Synchronize signal according to timestamps.
+
+    Parameters
+    ----------
+    x : ndarray
+        Signal.
+    timestamps : list
+        List of (timestamp, index) tuples.
+    samplerate : int
+        Samplerate of output
+
+    Returns
+    -------
+    out : ndarray
+        Signal syncronized to `timestamps` and resampled to `samplerate`.
+
+    """
+    # beginning and end of time code recording
+    min_time = timestamps[0][0]
+    max_time = timestamps[-1][0]
+
+    if start_time is None:
+        start_time = min_time
+    if end_time is None:
+        end_time = max_time
+
+    if not (min_time <= start_time and start_time < end_time and end_time <= max_time):
+        raise ValueError("Invalid start or end times.")
+
+    # filter time stamps according to user input times
+    filtered_timestamps = timestamps[
+        np.logical_and(start_time <= timestamps[:, 0], timestamps[:, 0] <= end_time)
+    ]
+
+    curser = 0
+    if out is None:
+        out = np.zeros(int((end_time - start_time).total_seconds()) * samplerate)
+
+    for j in tqdm(range(len(filtered_timestamps) - 1)):
+        chunk_start_time, chunk_start_sample = filtered_timestamps[j]
+        chunk_end_time, chunk_end_sample = filtered_timestamps[j + 1]
+
+        chunk = x[chunk_start_sample:chunk_end_sample]
+        chunk_seconds = (chunk_end_time - chunk_start_time).total_seconds()
+
+        if len(chunk) != samplerate:
+            chunk = signal.resample(chunk, int(chunk_seconds) * samplerate)
+        else:
+            chunk = chunk
+
+        out[curser : curser + len(chunk)] = chunk
+        curser += len(chunk)
+
+    return out
+
+
+def sync_folder_to_timestamps(
+    folder_name,
+    start_time,
+    end_time,
+    resamplerate=48000,
+    tzinfo=timezone.utc,
+    out_folder=None,
+    n_jobs=1
+):
+    """Read multiple wav recordings *rc.wav from the given folder,
+    Slice files for given interval (in time).
+    Save as .npz
+
+    Parameters
+    ----------
+    folder_name : folder with fullpath, ex) 'D:/ASFC/Roskilde/darkzone'
+
+    save
+    -------
+    saves rc_files, namelist_tc, audio chunks matrix as .npz file
+    filename: startT_endT_aligned.npz
+
+    """
+    folder = Path(folder_name)
+    if not folder.is_dir():
+        raise FileNotFoundError(folder_name)
+
+    if out_folder is not None:
+        out_folder = Path(out_folder)
+        if not out_folder.is_dir():
+            raise FileNotFoundError(out_folder)
+    else:
+        out_folder = folder
+
+    # load timestamps
+    timestamps_file = next(folder.glob("*_timestamps.npz"))
+    with np.load(timestamps_file, allow_pickle=True) as data:
+        timestamps = data["timestamps"]
+        samplerate = data["samplerate"]
+
+    if resamplerate:
+        samplerate = resamplerate
+
+    print(f"Using {timestamps_file}")
+
+    # beginning and end of time code recording
+    start_time = datetime.fromisoformat(start_time).replace(tzinfo=tzinfo)
+    end_time = datetime.fromisoformat(end_time).replace(tzinfo=tzinfo)
+
+    rc_files = sorted(list(folder.glob("*rc.wav")))
+
+    def syncer(rc_file):
+        print(f"Resyncing {rc_file}...")
+
+        # Read Audio
+        y, _ = sf.read(str(rc_file))
+        synced = sync_to_timestamps(y, timestamps, samplerate, start_time, end_time)
+        del y
+
+        out_name = rc_file.stem + "_synced" + rc_file.suffix
+        print(f"Writing {out_name}...")
+        sf.write(str(out_folder / out_name), synced, samplerate)
+
+    if n_jobs > 1:
+        Parallel(n_jobs=n_jobs)(delayed(syncer)(rc_file) for rc_file in rc_files)
+    else:
+        for i, rc_file in enumerate(rc_files):
+            syncer(rc_file)
